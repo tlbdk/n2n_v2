tlbdk/n2n_v2

commit a0da79d1c370a2074952610aef6ac5c92809ea5a

Partial work on supporting mingw. Refactoring conditional includes.
git-svn-id: https://svn.ntop.org/svn/ntop/trunk/n2n/n2n_v2@4036 a02cd0c1-8e76-42e1-a119-56f9641475e2

n2n.h
@@ -49,8 +49,6 @@
 #ifdef WIN32
 #include "win32/n2n_win32.h"
 #undef N2N_HAVE_DAEMON
-#else
-#include <getopt.h>
 #endif
 
 #include <time.h>
@@ -60,6 +58,10 @@
 #ifndef WIN32
 #include <netdb.h>
 #endif
+
+#ifndef _MSC_VER
+#include <getopt.h>
+#endif /* #ifndef _MSC_VER */
 
 #include <stdio.h>
 #include <errno.h>
@@ -73,7 +75,6 @@
 #include <pthread.h>
 
 #ifdef __linux__
-#define N2N_CAN_NAME_IFACE 1
 #include <linux/if.h>
 #include <linux/if_tun.h>
 #endif /* #ifdef __linux__ */
@@ -84,10 +85,6 @@
 
 #include <syslog.h>
 #include <sys/wait.h>
-
-#ifdef __sun__ /* Added for OpenSolaris rather than SPARC */
-#undef N2N_HAVE_DAEMON 
-#endif /* #ifdef __sun__ */
 
 #define ETH_ADDR_LEN 6
 struct ether_hdr

n2n_wire.h
@@ -9,13 +9,19 @@
 #define N2N_WIRE_H_
 
 #include <stdlib.h>
-#ifndef WIN32
+
+#if defined(WIN32)
+#include "win32/n2n_win32.h"
+
+#if defined(__MINGW32__)
+#include <stdint.h>
+#endif /* #ifdef __MINGW32__ */
+
+#else /* #if defined(WIN32) */
 #include <stdint.h>
 #include <netinet/in.h>
 #include <sys/socket.h> /* AF_INET and AF_INET6 */
-#else
-#include "win32/n2n_win32.h"
-#endif
+#endif /* #if defined(WIN32) */
 
 #define N2N_PKT_VERSION                 2
 #define N2N_DEFAULT_TTL                 2       /* can be forwarded twice at most */

win32/n2n_win32.h
@@ -13,31 +13,26 @@
 
 #include <winsock2.h>
 #include <windows.h>
+#include <winioctl.h>
 
 #include "wintap.h"
 
+#ifdef _MSC_VER
 #include "getopt.h"
 
-typedef unsigned long in_addr_t;
-typedef __int8 int8_t;
-typedef __int16 int16_t;
-typedef __int32 int32_t;
-typedef __int64 int64_t;
-typedef unsigned __int8 uint8_t;
-typedef unsigned __int16 uint16_t;
-typedef unsigned __int32 uint32_t;
-typedef unsigned __int64 uint64_t;
+/* Other Win environments are expected to support stdint.h */
+typedef unsigned int u_int32_t;
+typedef unsigned short u_int16_t;
+typedef unsigned char u_int8_t;
 typedef int ssize_t;
+#endif /* #ifdef _MSC_VER */
+
+typedef unsigned long in_addr_t;
+
 
 #define EAFNOSUPPORT   WSAEAFNOSUPPORT 
 #define MAX(a,b) (a > b ? a : b)
 #define MIN(a,b) (a < b ? a : b)
-typedef unsigned int u_int32_t;
-typedef unsigned short u_int16_t;
-typedef unsigned char u_int8_t;
-typedef int int32_t;
-typedef short int16_t;
-typedef char int8_t;
 
 #define snprintf _snprintf
 #define strdup _strdup

edge.c
@@ -468,17 +468,20 @@ static void help() {
   print_n2n_version();
 
   printf("edge "
-#ifdef __linux__
+#if defined(N2N_CAN_RENAME_IFACE)
 	 "-d <tun device> "
-#endif
+#endif /* #if defined(N2N_CAN_RENAME_IFACE) */
 	 "-a [static:|dhcp:]<tun IP address> "
 	 "-c <community> "
 	 "[-k <encrypt key> | -K <key file>] "
 	 "-s <netmask> "
-#ifndef WIN32
+#if defined(N2N_HAVE_SETUID)
 	 "[-u <uid> -g <gid>]"
+#endif /* #ifndef N2N_HAVE_SETUID */
+
+#if defined(N2N_HAVE_DAEMON)
 	 "[-f]"
-#endif
+#endif /* #if defined(N2N_HAVE_DAEMON) */
 	 "[-m <MAC address>]"
 	 "\n"
 	 "-l <supernode host:port> "

n2n_keyfile.c
@@ -14,7 +14,7 @@
 #ifdef WIN32
 char *strsep( char **ppsz_string, const char *psz_delimiters )
 {
-	char *p;
+    char *p;
     char *psz_string = *ppsz_string;
     if( !psz_string )
         return NULL;

twofish.h
@@ -49,14 +49,16 @@
 #include "win32/n2n_win32.h"
 #endif
 
-/* Not shipped with Visual Studio (as stated by the stdint.h wikipedia page) */
 #ifndef _MSC_VER
+/* Not shipped with Visual Studio (as stated by the stdint.h wikipedia page) */
 #include <stdint.h> /* defines uintN_t types */
 #endif
-/* The following are redefinitions if sys/types.h has been included too. That's
- * OK.*/
+
+#ifdef __sun__ /* Should be HAVE_SYS_TYPES */
+/* The following are redefinitions if sys/types.h has been included too.*/
 typedef uint32_t u_int32_t;
 typedef uint8_t  u_int8_t;
+#endif /* #ifdef __sun__ */
 
 /* Constants */
 

commit 1b5a4c19d859fe9dbcb8c55cf32b37a03a158e9c

Follow up on r4036. Attempt to fix things broken in r4036.
git-svn-id: https://svn.ntop.org/svn/ntop/trunk/n2n/n2n_v2@4045 a02cd0c1-8e76-42e1-a119-56f9641475e2

n2n.c
@@ -140,7 +140,7 @@ void traceEvent(int eventTraceLevel, char* file, int line, char * format, ...) {
 /* addr should be in network order. Things are so much simpler that way. */
 char* intoa(uint32_t /* host order */ addr, char* buf, uint16_t buf_len) {
   char *cp, *retStr;
-  uint8_t byte;
+  uint8_t byteval;
   int n;
 
   cp = &buf[buf_len];
@@ -148,14 +148,14 @@ char* intoa(uint32_t /* host order */ addr, char* buf, uint16_t buf_len) {
 
   n = 4;
   do {
-    byte = addr & 0xff;
-    *--cp = byte % 10 + '0';
-    byte /= 10;
-    if (byte > 0) {
-      *--cp = byte % 10 + '0';
-      byte /= 10;
-      if (byte > 0)
-        *--cp = byte + '0';
+    byteval = addr & 0xff;
+    *--cp = byteval % 10 + '0';
+    byteval /= 10;
+    if (byteval > 0) {
+      *--cp = byteval % 10 + '0';
+      byteval /= 10;
+      if (byteval > 0)
+        *--cp = byteval + '0';
     }
     *--cp = '.';
     addr >>= 8;

n2n.h
@@ -98,6 +98,7 @@ typedef struct ether_hdr ether_hdr_t;
 
 #ifdef __sun__
 #include <sys/sysmacros.h> /* MIN() and MAX() declared here */
+#undef N2N_HAVE_DAEMON
 #endif /* #ifdef __sun__ */
 
 #include <netinet/in.h>

n2n_wire.h
@@ -14,6 +14,7 @@
 #include "win32/n2n_win32.h"
 
 #if defined(__MINGW32__)
+#define _WIN32_WINNT 0x501 //Otherwise the linker doesnt find getaddrinfo
 #include <stdint.h>
 #endif /* #ifdef __MINGW32__ */
 

win32/n2n_win32.h
@@ -21,9 +21,17 @@
 #include "getopt.h"
 
 /* Other Win environments are expected to support stdint.h */
+
+/* stdint.h typedefs (C99) (not present in Visual Studio) */
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+
+/* sys/types.h typedefs (not present in Visual Studio) */
 typedef unsigned int u_int32_t;
 typedef unsigned short u_int16_t;
 typedef unsigned char u_int8_t;
+
 typedef int ssize_t;
 #endif /* #ifdef _MSC_VER */
 